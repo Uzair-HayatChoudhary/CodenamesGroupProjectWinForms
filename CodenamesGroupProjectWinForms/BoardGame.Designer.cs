93ce9f94f9a929925ecaac53cee22e1aa1be0abc
Uzair-Hayat Choudhary <uzair.chou01@gmail.com>
2022-02-18 19:43:52 -0800

    Rules Page, main board game ui
    design for rules page done, could change colors. created link to main game "screen",

---
+++ CodenamesGroupProjectWinForms/BoardGame.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CodenamesGroupProjectWinForms
+{
+    partial class BoardGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 28.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(610, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(453, 85);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "\"Team turn\"";
+            // 
+            // BoardGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1810, 1151);
+            this.Controls.Add(this.label1);
+            this.Name = "BoardGame";
+            this.Text = "BoardGame";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
--- CodenamesGroupProjectWinForms/BoardGame.cs
+++ CodenamesGroupProjectWinForms/BoardGame.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace CodenamesGroupProjectWinForms
 {
-    public partial class Form1 : Form
+    public partial class BoardGame : Form
     {
-        public Form1()
+        public BoardGame()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
